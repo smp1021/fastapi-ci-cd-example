16b6f817c08c1614c7a87c5282cf8739640a4c3f
smp1021 <samuel21.marmolejo@gmail.com>
2025-11-06 18:15:55 -0500

    feat: Añadir endpoint /saludo y sus pruebas

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello CI/CD"}
+
+
+@app.get("/saludo")
+def read_saludo(nombre: str = "Mundo"):
+    return {"message": f"Hola, {nombre}"}
--- test/test_main.py
+++ test/test_main.py
@@ -8,3 +8,17 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello CI/CD"}
+
+
+def test_read_saludo_default():
+    # Prueba el endpoint SIN enviar un nombre
+    response = client.get("/saludo")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hola, Mundo"}
+
+
+def test_read_saludo_with_name():
+    # Prueba el endpoint ENVIANDO un nombre
+    response = client.get("/saludo?nombre=Gemini")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hola, Gemini"}
